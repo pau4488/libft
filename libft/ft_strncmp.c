8233a6c21cce3a15f829e8cf1d56a4486fc158be
Pau Brossa Perez <paubrossa@gmail.com>
2022-12-09 02:09:04 +0100

    strrchr

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 01:44:57 by pbrossa-          #+#    #+#             */
-/*   Updated: 2022/12/09 02:04:10 by pbrossa-         ###   ########.fr       */
+/*   Updated: 2022/12/09 02:08:28 by pbrossa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ int strncmp(const char *s1, const char *s2, size_t n)
 
     i = 0;
 
-    while (s1[i] == s2[i])
+    while(s1[i] == s2[i])
     {
-        
+        if(s1[i] == s2[i] && s1[i] == '\0')
+            return (0);
     }
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
