0268262a18284b336a0f7c9c6096208b9f6d4539
Pau Brossa Perez <paubrossa@gmail.com>
2022-12-12 13:08:21 +0100

    mati

---
+++ libft/ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 12:37:59 by pbrossa-          #+#    #+#             */
+/*   Updated: 2022/12/12 12:38:53 by pbrossa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*result;
+
+	result = malloc(count * size);
+	if (!result)
+		return (NULL);
+	else
+	{
+		ft_bzero(result, (count * size));
+		return (result);
+	}
+}
---
+++ libft/ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 12:46:19 by pbrossa-          #+#    #+#             */
+/*   Updated: 2022/12/12 12:47:36 by pbrossa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (ft_strrchr(set, s1[len]) && len)
+		len--;
+	return (ft_substr(s1, 0, len + 1));
+}
