48c941b6a7ed383320098569bb918b6980c154a1
Pau Brossa Perez <paubrossa@gmail.com>
2022-12-16 17:11:01 +0100

    ff

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:09:01 by pbrossa-          #+#    #+#             */
-/*   Updated: 2022/12/14 19:47:34 by pbrossa-         ###   ########.fr       */
+/*   Updated: 2022/12/16 16:28:12 by pbrossa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 int	ft_isalpha(int c)
 {
-	if(ft_isupper || ft_islower)
-		return(1);
-	return(0);
+	if (ft_isupper || ft_islower)
+		return (1);
+	return (0);
 }
 
-int	ft_isupper(int c)
+static int	ft_isupper(int c)
 {
-	if(c >= 65 && c <= 90)
-		return(1);
-	return(0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
 }
 
-int	ft_islower(int c)
+static int	ft_islower(int c)
 {
-	if(c >= 97 && c <= 122)
-		return(1);
-	return(0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
 }
