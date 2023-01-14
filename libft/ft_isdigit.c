2f39244a396403c4b0e2fe26caec964307b6bf23
Pau Brossa Perez <paubrossa@gmail.com>
2023-01-14 02:37:27 +0100

    f

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/14 19:09:01 by pbrossa-          #+#    #+#             */
-/*   Updated: 2023/01/14 02:35:28 by pbrossa-         ###   ########.fr       */
+/*   Created: 2023/01/14 02:25:14 by pbrossa-          #+#    #+#             */
+/*   Updated: 2023/01/14 02:31:24 by pbrossa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isupper(int c);
-static int	ft_islower(int c);
-
-int	ft_isalpha(int c)
+int	ft_isdigit()
 {
-	if (ft_isupper(c) || ft_islower(c))
-		return (1);
-	return (0);
-}
-
-static int	ft_isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-		return (1);
-	return (0);
-}
-
-static int	ft_islower(int c)
-{
-	if (c >= 97 && c <= 122)
-		return (1);
-	return (0);
+	return 0;
 }
