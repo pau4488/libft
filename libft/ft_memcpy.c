f589c5093805e47beacc03e68225335b4ea4e680
Pau Brossa Perez <paubrossa@gmail.com>
2023-01-19 22:16:54 +0100

    paco OK fins memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 13:41:51 by pbrossa-          #+#    #+#             */
-/*   Updated: 2023/01/19 21:33:45 by pbrossa-         ###   ########.fr       */
+/*   Updated: 2023/01/19 22:15:58 by pbrossa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (dest == NULL && src == NULL)
+		return (dest);
 	while (i < n)
 	{
 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-		dest++;
-		src++;
+		i++;
 	}
 	return (dest);
 }
