92d7936e03030dbe9f2418c8faddeb473bd182b4
pau4488 <paubrossa@gmail.com>
2023-01-26 12:52:49 +0100

    strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -20,7 +20,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	i = 0;
 	j = 0;
 	if (!little)
-		return (big);
+		return ((char *)big);
 	while (i < len && little[j] != '\0')
 	{
 		if (big[i] == little[j])
@@ -29,7 +29,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 			j = 0;
 		i++;
 	}
-	if (j == len)
-		return big[i - j]
+	if (j == (ft_strlen(little)))
+		return (&((char *)big)[i - j]);
 	return (NULL);
 }
