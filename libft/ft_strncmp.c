94cb2b892ad76193903b3f8952657b5d3ce8cd1c
Pau Brossa Perez <paubrossa@gmail.com>
2023-01-26 21:13:43 +0100

    revisar strlcat

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 20:40:30 by pbrossa-          #+#    #+#             */
-/*   Updated: 2023/01/25 20:40:30 by pbrossa-         ###   ########.fr       */
+/*   Updated: 2023/01/26 19:03:44 by pbrossa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] == s2[i] && s1[i] != '\0')
 			i++;
 		else
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	}
 	return (0);
 }
