eede30904e6ac268f90c8321d32e9794ab759a17
Pau Brossa Perez <paubrossa@gmail.com>
2023-03-11 20:58:02 +0100

    final

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,61 +6,51 @@
 /*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 10:52:14 by pbrossa-          #+#    #+#             */
-/*   Updated: 2023/03/03 10:52:14 by pbrossa-         ###   ########.fr       */
+/*   Updated: 2023/03/11 20:48:04 by pbrossa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strrev(char *str);
+static int	ft_numlen(int n);
 
 char	*ft_itoa(int n)
 {
 	char	*str;
-	int		i;
+	int		len;
+	int		sign;
 
-	i = 0;
-	str = malloc(100 * sizeof(char));
+	sign = 1;
 	if (n < 0)
+		sign = -1;
+	len = ft_numlen(n);
+	str = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	if (n == 0)
+		str[0] = '0';
+	if (sign < 0)
+		str[0] = '-';
+	while (n != 0)
 	{
-		str[i++] = '-';
-		n = -n;
-	}
-	else if (n == 0)
-	{
-		str[i++] = '0';
-		str[i] = '\0';
-		return (str);
-	}
-	while ((n / 10) > 0)
-	{
-		str[i++] = n % 10 + '0';
+		len--;
+		str[len] = (sign * (n % 10)) + '0';
 		n /= 10;
 	}
-	if (n > 0)
-		str[i++] = n + '0';
-	str[i] = '\0';
-	return (ft_strrev(str));
+	return (str);
 }
 
-static char	*ft_strrev(char *str)
+static int	ft_numlen(int n)
 {
-	char	temp;
-	size_t	i;
-	size_t	j;
+	int	len;
 
-	temp = ' ';
-	i = 0;
-	j = ft_strlen(str) - 1;
-	if (str[i] == '-')
-		i++;
-	while (i < ft_strlen(str) - 1 && i < j)
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	while (n != 0)
 	{
-		temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-		i++;
-		j--;
+		len++;
+		n /= 10;
 	}
-	return (str);
+	return (len);
 }
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pbrossa- <pbrossa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 22:44:36 by pbrossa-          #+#    #+#             */
-/*   Updated: 2023/03/10 01:10:58 by pbrossa-         ###   ########.fr       */
+/*   Updated: 2023/03/11 20:53:27 by pbrossa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trmd;
-	int		len;
+	size_t	i;
 
-	len = ft_strlen(s1);
-	trmd = malloc(ft_strlen(s1) * sizeof(char));
-	if (!trmd || !s1 || !set)
-		return (NULL);
+	if (!s1 || !set)
+		return (0);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
-	while (len > 0 && ft_strrchr(set, s1[len - 1]))
-		len--;
-	trmd = ft_substr(s1, 0, len);
-	return (trmd);
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
 }
