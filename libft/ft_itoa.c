733e1d8dac290a3b0b2bc6f15ceafd636c5dc146
pau4488 <paubrossa@gmail.com>
2023-03-11 18:52:15 +0100

    wsertg

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -12,97 +12,55 @@
 
 #include "libft.h"
 
-static char *ft_strrev(char *str);
+static char	*ft_strrev(char *str);
 
 char	*ft_itoa(int n)
 {
-    char    *str;
-    int     i;
+	char	*str;
+	int		i;
 
-    i = 0;
-    str = malloc(100 * sizeof(char));
+	i = 0;
+	str = malloc(100 * sizeof(char));
 	if (n < 0)
-    {
-        str[i] = '-';
-        i++;
-        n = -n;
-    }
-    else if (n == 0)
-    {
-        str[i++] = '0';
-        str[i] = '\0';
-        return (str);
-    }
-    while ((n / 10) > 0)
-    {
-        str[i] = n % 10 + '0';
-        n /= 10;
-        i++;
-    }
-    if (n > 0)
-        str[i++] = n + '0';
-    str[i] = '\0';
-    return (ft_strrev(str));
+	{
+		str[i++] = '-';
+		n = -n;
+	}
+	else if (n == 0)
+	{
+		str[i++] = '0';
+		str[i] = '\0';
+		return (str);
+	}
+	while ((n / 10) > 0)
+	{
+		str[i++] = n % 10 + '0';
+		n /= 10;
+	}
+	if (n > 0)
+		str[i++] = n + '0';
+	str[i] = '\0';
+	return (ft_strrev(str));
 }
 
-static char *ft_strrev(char *str)
+static char	*ft_strrev(char *str)
 {
-    char    temp;
-    size_t     i;
-    size_t     j;
+	char	temp;
+	size_t	i;
+	size_t	j;
 
-    temp = ' ';
-    i = 0;
-    j = ft_strlen(str) - 1;
-    if (str[i] == '-')
-        i++;
-    while (i < ft_strlen(str) - 1 && i < j)
-    {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-        i++;
-        j--;
-    }
-    return (str);
+	temp = ' ';
+	i = 0;
+	j = ft_strlen(str) - 1;
+	if (str[i] == '-')
+		i++;
+	while (i < ft_strlen(str) - 1 && i < j)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++;
+		j--;
+	}
+	return (str);
 }
-
-//Passar per Python C debugger online
-/*-----------------------------------------------------------------------
-#include <stdio.h>
-#include <string.h>
-
-void reverse(char *str) {
-    int i, j;
-    char temp;
-    for (i = 0, j = strlen(str) - 1; i < j; i++, j--) {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-    }
-}
-
-void itoa(int n, char *str) {
-    int i, sign;
-    if ((sign = n) < 0) {
-        n = -n;
-    }
-    i = 0;
-    do {
-        str[i++] = n % 10 + '0';
-    } while ((n /= 10) > 0);
-    if (sign < 0) {
-        str[i++] = '-';
-    }
-    str[i] = '\0';
-    reverse(str);
-}
-
-int main() {
-    int num = -123;
-    char str[12];
-    itoa(num, str);
-    printf("The integer %d is converted to string %s\n", num, str);
-    return 0;
-}
------------------------------------------------------------------------*/
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -11,9 +11,76 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-char **ft_split(char const *s, char c)
+
+static int		count_words(char const *str, char delim);
+static size_t	word_len(char const *str, char delim);
+static void		lines(char **result, char const *s, char c);
+
+char	**ft_split(char const *s, char c)
 {
-	
+	char	**result;
+	int		word_count;
+
+	word_count = count_words(s, c);
+	result = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	lines(result, s, c);
+	return (result);
 }
-*/
+
+static void	lines(char **result, char const *s, char c)
+{
+	int		i;
+	size_t	len;
+
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = word_len(s, c);
+			result[i] = (char *)malloc((len + 1) * sizeof(char));
+			if (!result[i])
+				return ;
+			ft_strlcpy(result[i], s, len + 1);
+			i++;
+			s += len;
+		}
+		else
+			s++;
+	}
+	result[i] = NULL;
+}
+
+static int	count_words(char const *str, char delim)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		if (*str != delim)
+		{
+			count++;
+			while (*str && *str != delim)
+				str++;
+		}
+		else
+			str++;
+	}
+	return (count);
+}
+
+static size_t	word_len(char const *str, char delim)
+{
+	size_t	len;
+
+	len = 0;
+	while (*str && *str != delim)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
